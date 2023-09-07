f931af9ef181776b40eb92becb99ccb4517f197f
dave8git <dave8git@gmail.com>
2023-09-07 17:13:15 +0200

    Fix catch updated event

--- src/js/script.js
+++ src/js/script.js
@@ -8,7 +8,11 @@
     },
     containerOf: {
       books: '.books-list',
+    },
+    class: {
+      favorite: '',
     }
+    
   };
   const templates = {
     book: Handlebars.compile(document.querySelector(select.templateOf.book).innerHTML),
@@ -19,6 +23,8 @@
       const thisBook = this;
       thisBook.data = data;
       thisBook.render();
+      thisBook.getElements();
+      thisBook.initActions();
     }
     render() {
       const thisBook = this;
@@ -28,32 +34,64 @@
       bookContainer.appendChild(thisBook.element);
     }
 
+    getElements() {
+      const thisBook = this;
+      thisBook.cover = thisBook.element.querySelector('.book__image');
+    }
+
+    initActions() {
+      const thisBook = this; 
+      thisBook.cover.addEventListener('click', function (event) {
+        event.preventDefault();
+        thisBook.announce();
+      });
+    }
+
+    announce() {
+      const thisWidget = this;
+      const event = new CustomEvent('updated', {
+        bubbles: true
+      });
+      thisWidget.element.dispatchEvent(event);
+      //console.log(event);
+    }
 
   }
   const app = {
 
-    initData: function() {
-      const thisApp = this; 
+    initData: function () {
+      const thisApp = this;
 
       thisApp.data = dataSource;
     },
 
-    initBooks: function() {
+    initBooks: function () {
       const thisApp = this;
+      const books = [];
+      const favoriteBooks = [];
 
-      for(let bookData of thisApp.data.books) {
-        new Book(bookData) ;
+     
+      
+      for (let bookData of thisApp.data.books) {
+        books.push(new Book(bookData));
+        //console.log(new Book(bookData));
       }
+
+      books.map((book) => {
+        book.addEventListener('updated', function () {
+          thisApp.addToFavorites();
+        });
+      });
     },
 
     init: function () {
       const thisApp = this;
       thisApp.initData();
       thisApp.initBooks();
-    
+
     }
   };
-    
+
   app.init();
 }
 
